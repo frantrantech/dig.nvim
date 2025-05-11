8b45907a4227939862cd2322e05cdc6b61828354
frantrantech <francuhz4@gmail.com>
2025-05-10 22:21:11 -0500

    ESC to close window. Can create/read lines in window. Update lines WIP

--- lua/dig.lua
+++ lua/dig.lua
@@ -1,23 +1,17 @@
 local M = {}
-local finders = require "telescope.finders"
-local make_entry = require "telescope.make_entry"
-local pickers = require "telescope.pickers"
-local utils = require "telescope.utils"
-local conf = require("telescope.config").values
-local log = require "telescope.log"
 local popup = require("plenary.popup")
 
-local flatten = utils.flatten
+Dig_window = nil
+Dig_window_id = nil
 
-window = nil
-window_id = nil
-
+-- Creates a window, sets Dig_window_id to be used by close_window()
+-- Returns the window_id and window to ensure that toggle() works correctly
 local function create_window()
   local window_buffer = vim.api.nvim_create_buf(false, false)
-  local width = 60
-  local height = 10
+  local width = 70
+  local height = 20
   local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
-  window_id, window = popup.create(window_buffer, {
+  Dig_window_id, Dig_window = popup.create(window_buffer, {
     title = "Dig",
     highlight = "DigWindow",
     line = math.floor(((vim.o.lines - height) / 2) - 1),
@@ -27,181 +21,47 @@ local function create_window()
     borderchars = borderchars,
   })
   vim.api.nvim_win_set_option(
-    window.border.win_id,
-    "winhl",     -- Highlights
+    Dig_window.border.win_id,
+    "winhl", -- Highlights
     "Normal:DigBorder"
   )
+
+  return {
+    win_buf = window_buffer,
+    win_id = Dig_window_id
+  }
 end
 
+--Dig_window_id should be non null here 
 local function close_window()
-  vim.api.nvim_win_close(window_id, true)
+  vim.api.nvim_win_close(Dig_window_id, true)
 end
 
+-- Closes window and returns if window is open
+--
+-- Creates a window if it isn't open.
+-- Add <ESC> command in the new buffer to toggle (close) the window
 function M.toggle_window()
-  local window_is_open = window_id ~= nil and vim.api.nvim_win_is_valid(window_id)
+  local window_is_open = Dig_window_id ~= nil and vim.api.nvim_win_is_valid(Dig_window_id)
   if window_is_open then
     close_window()
+    return
   else
-    create_window()
+    local win = create_window()
+    local win_buf = win.win_buf
+    vim.api.nvim_buf_set_keymap(win_buf, 'n', '<ESC>', '<Cmd>lua require("dig").toggle_window()<CR>',
+      { silent = true })
+
+-- 0 Based indexing in nvim api
+-- 1 based indexing with native lua
+    -- vim.api.nvim_buf_set_lines(win_buf,0,0,true,{"Hello World"})
+    -- vim.api.nvim_buf_set_lines(win_buf,1,1,true,{"Hello World2"})
+    -- local line_ct = vim.api.nvim_buf_line_count(win_buf)
+    -- local line1 = vim.api.nvim_buf_get_lines(win_buf, 1, 2, true)
+    -- print(line1[1])
+
   end
 end
 
-
-
-
--- local files = {}
---
--- function M.find_files(opts)
--- -- files.find_files = function(opts)
---   local find_command = (function()
---     if opts.find_command then
---       if type(opts.find_command) == "function" then
---         return opts.find_command(opts)
---       end
---       return opts.find_command
---     elseif 1 == vim.fn.executable "rg" then
---       return { "rg", "--files", "--color", "never" }
---     elseif 1 == vim.fn.executable "fd" then
---       return { "fd", "--type", "f", "--color", "never" }
---     elseif 1 == vim.fn.executable "fdfind" then
---       return { "fdfind", "--type", "f", "--color", "never" }
---     elseif 1 == vim.fn.executable "find" and vim.fn.has "win32" == 0 then
---       return { "find", ".", "-type", "f" }
---     elseif 1 == vim.fn.executable "where" then
---       return { "where", "/r", ".", "*" }
---     end
---   end)()
---
---   if not find_command then
---     utils.notify("builtin.find_files", {
---       msg = "You need to install either find, fd, or rg",
---       level = "ERROR",
---     })
---     return
---   end
---
---   local command = find_command[1]
---   local hidden = opts.hidden
---   local no_ignore = opts.no_ignore
---   local no_ignore_parent = opts.no_ignore_parent
---   local follow = opts.follow
---   local search_dirs = opts.search_dirs
---   local search_file = opts.search_file
---
---   if search_dirs then
---     for k, v in pairs(search_dirs) do
---       search_dirs[k] = utils.path_expand(v)
---     end
---   end
---
---   if command == "fd" or command == "fdfind" or command == "rg" then
---     if hidden then
---       find_command[#find_command + 1] = "--hidden"
---     end
---     if no_ignore then
---       find_command[#find_command + 1] = "--no-ignore"
---     end
---     if no_ignore_parent then
---       find_command[#find_command + 1] = "--no-ignore-parent"
---     end
---     if follow then
---       find_command[#find_command + 1] = "-L"
---     end
---     if search_file then
---       if command == "rg" then
---         find_command[#find_command + 1] = "-g"
---         find_command[#find_command + 1] = "*" .. search_file .. "*"
---       else
---         find_command[#find_command + 1] = search_file
---       end
---     end
---     if search_dirs then
---       if command ~= "rg" and not search_file then
---         find_command[#find_command + 1] = "."
---       end
---       vim.list_extend(find_command, search_dirs)
---     end
---   elseif command == "find" then
---     if not hidden then
---       table.insert(find_command, { "-not", "-path", "*/.*" })
---       find_command = flatten(find_command)
---     end
---     if no_ignore ~= nil then
---       log.warn "The `no_ignore` key is not available for the `find` command in `find_files`."
---     end
---     if no_ignore_parent ~= nil then
---       log.warn "The `no_ignore_parent` key is not available for the `find` command in `find_files`."
---     end
---     if follow then
---       table.insert(find_command, 2, "-L")
---     end
---     if search_file then
---       table.insert(find_command, "-name")
---       table.insert(find_command, "*" .. search_file .. "*")
---     end
---     if search_dirs then
---       table.remove(find_command, 2)
---       for _, v in pairs(search_dirs) do
---         table.insert(find_command, 2, v)
---       end
---     end
---   elseif command == "where" then
---     if hidden ~= nil then
---       log.warn "The `hidden` key is not available for the Windows `where` command in `find_files`."
---     end
---     if no_ignore ~= nil then
---       log.warn "The `no_ignore` key is not available for the Windows `where` command in `find_files`."
---     end
---     if no_ignore_parent ~= nil then
---       log.warn "The `no_ignore_parent` key is not available for the Windows `where` command in `find_files`."
---     end
---     if follow ~= nil then
---       log.warn "The `follow` key is not available for the Windows `where` command in `find_files`."
---     end
---     if search_dirs ~= nil then
---       log.warn "The `search_dirs` key is not available for the Windows `where` command in `find_files`."
---     end
---     if search_file ~= nil then
---       log.warn "The `search_file` key is not available for the Windows `where` command in `find_files`."
---     end
---   end
---
---   if opts.cwd then
---     opts.cwd = utils.path_expand(opts.cwd)
---   end
---
---   opts.entry_maker = opts.entry_maker or make_entry.gen_from_file(opts)
---
---   pickers
---     .new(opts, {
---       prompt_title = "Find Files",
---       __locations_input = true,
---       finder = finders.new_oneshot_job(find_command, opts),
---       previewer = conf.grep_previewer(opts),
---       sorter = conf.file_sorter(opts),
---     })
---     :find()
--- end
---
---
---
--- local function apply_checks(mod)
---   for k, v in pairs(mod) do
---     mod[k] = function(opts)
---       opts = opts or {}
---
---       v(opts)
---     end
---   end
---
---   return mod
--- end
---
--- apply_checks(files)
-
 vim.keymap.set('n', '<leader>-', '<Cmd>lua require("dig").toggle_window()<CR>')
--- vim.keymap.set('n', '<leader>=', '<Cmd>lua require("dig").find_files()<CR>',{hidden=true, layout_config={prompt_position="top"}})
-
-
-
 return M
---
+++ lua/picker.lua
@@ -0,0 +1,154 @@
+local finders = require "telescope.finders"
+local make_entry = require "telescope.make_entry"
+local pickers = require "telescope.pickers"
+local conf = require("telescope.config").values
+local log = require "telescope.log"
+local utils = require "telescope.utils"
+local flatten = utils.flatten
+
+local M = {}
+local files = {}
+
+local function find(opts)
+  local find_command = (function()
+    if opts.find_command then
+      if type(opts.find_command) == "function" then
+        return opts.find_command(opts)
+      end
+      return opts.find_command
+    elseif 1 == vim.fn.executable "rg" then
+      return { "rg", "--files", "--color", "never" }
+    elseif 1 == vim.fn.executable "fd" then
+      return { "fd", "--type", "f", "--color", "never" }
+    elseif 1 == vim.fn.executable "fdfind" then
+      return { "fdfind", "--type", "f", "--color", "never" }
+    elseif 1 == vim.fn.executable "find" and vim.fn.has "win32" == 0 then
+      return { "find", ".", "-type", "f" }
+    elseif 1 == vim.fn.executable "where" then
+      return { "where", "/r", ".", "*" }
+    end
+  end)()
+
+  if not find_command then
+    utils.notify("builtin.find_files", {
+      msg = "You need to install either find, fd, or rg",
+      level = "ERROR",
+    })
+    return
+  end
+
+  local command = find_command[1]
+  local hidden = opts.hidden
+  local no_ignore = opts.no_ignore
+  local no_ignore_parent = opts.no_ignore_parent
+  local follow = opts.follow
+  local search_dirs = opts.search_dirs
+  local search_file = opts.search_file
+
+  if search_dirs then
+    for k, v in pairs(search_dirs) do
+      search_dirs[k] = utils.path_expand(v)
+    end
+  end
+  if command == "fd" or command == "fdfind" or command == "rg" then
+    if hidden then
+      find_command[#find_command + 1] = "--hidden"
+    end
+    if no_ignore then
+      find_command[#find_command + 1] = "--no-ignore"
+    end
+    if no_ignore_parent then
+      find_command[#find_command + 1] = "--no-ignore-parent"
+    end
+    if follow then
+      find_command[#find_command + 1] = "-L"
+    end
+    if search_file then
+      if command == "rg" then
+        find_command[#find_command + 1] = "-g"
+        find_command[#find_command + 1] = "*" .. search_file .. "*"
+      else
+        find_command[#find_command + 1] = search_file
+      end
+    end
+    if search_dirs then
+      if command ~= "rg" and not search_file then
+        find_command[#find_command + 1] = "."
+      end
+      vim.list_extend(find_command, search_dirs)
+    end
+  elseif command == "find" then
+    if not hidden then
+      table.insert(find_command, { "-not", "-path", "*/.*" })
+      find_command = flatten(find_command)
+    end
+    if no_ignore ~= nil then
+      log.warn "The `no_ignore` key is not available for the `find` command in `find_files`."
+    end
+    if no_ignore_parent ~= nil then
+      log.warn "The `no_ignore_parent` key is not available for the `find` command in `find_files`."
+    end
+    if follow then
+      table.insert(find_command, 2, "-L")
+    end
+    if search_file then
+      table.insert(find_command, "-name")
+      table.insert(find_command, "*" .. search_file .. "*")
+    end
+    if search_dirs then
+      table.remove(find_command, 2)
+      for _, v in pairs(search_dirs) do
+        table.insert(find_command, 2, v)
+      end
+    end
+  elseif command == "where" then
+    if hidden ~= nil then
+      log.warn "The `hidden` key is not available for the Windows `where` command in `find_files`."
+    end
+    if no_ignore ~= nil then
+      log.warn "The `no_ignore` key is not available for the Windows `where` command in `find_files`."
+    end
+    if no_ignore_parent ~= nil then
+      log.warn "The `no_ignore_parent` key is not available for the Windows `where` command in `find_files`."
+    end
+    if follow ~= nil then
+      log.warn "The `follow` key is not available for the Windows `where` command in `find_files`."
+    end
+    if search_dirs ~= nil then
+      log.warn "The `search_dirs` key is not available for the Windows `where` command in `find_files`."
+    end
+    if search_file ~= nil then
+      log.warn "The `search_file` key is not available for the Windows `where` command in `find_files`."
+    end
+  end
+
+  if opts.cwd then
+    opts.cwd = utils.path_expand(opts.cwd)
+  end
+
+  opts.entry_maker = opts.entry_maker or make_entry.gen_from_file(opts)
+
+  pickers
+      .new(opts, {
+        prompt_title = "Ignore Find Files",
+        __locations_input = true,
+        finder = finders.new_oneshot_job(find_command, opts),
+        previewer = conf.grep_previewer(opts),
+        sorter = conf.file_sorter(opts),
+      })
+      :find()
+end
+
+function M.find_files()
+  print("here")
+  local opts = {
+    cwd = "~/Desktop/testing",
+    grep_open_files = false,
+    search = "*"
+  }
+  find(opts)
+end
+
+vim.keymap.set('n', '<leader>=', '<Cmd>lua require("picker").find_files()<CR>')
+
+return M
